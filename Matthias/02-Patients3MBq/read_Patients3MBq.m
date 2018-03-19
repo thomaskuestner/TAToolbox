33a790af25e6fec0f5d1ffff1e67da8aee41f72e
MatthiasWeinhold <m.weinhold@weinhold-smb.de>
2018-03-19 15:24:11 +0100

    read_Patients3MBq finished

--- Matthias/02-Patients3MBq/read_Patients3MBq.m
+++ Matthias/02-Patients3MBq/read_Patients3MBq.m
@@ -13,6 +13,7 @@ mask_Size=size(allROI_mask);
 single_ROIsmask={};
 temp=0;
 
+
 %% Create single ROIs from a mask
 for i=1:v_max
     ROI_indices=find(allROI_mask==i);
@@ -21,31 +22,53 @@ for i=1:v_max
         biggest_ROI=i;
         temp=length_ROI_indices;
     end
-    
+    [i_geometry,j_geometry,k_geometry]=ind2sub(size(allROI_mask), find(allROI_mask==i));
+    if all(k_geometry == k_geometry(1))
+        geometry{i,1}='flat_plate';
+    else
+        geometry{i,1}='3D';
+    end
     single_ROIsmask{i}=zeros(mask_Size);
     for j=1:length_ROI_indices
         single_ROIsmask{1,i}(ROI_indices(j))=1;
     end
-    single_ROIsmask{1,i}=logical(single_ROIsmask{1,i});
-    
+  
 end
-all_single_ROIs=single_ROIsmask;
 
-%% create mha-template 
-load('template_mha_mask.mat');
-output_mha_mask= template_mha_mask;
-%clear template_mha_mask;
-output_mha_mask.mask = all_single_ROIs{1,1};
-%prepare meshing
-    qoffset_sc(1,2)= mask_100_nii.hdr.hist.qoffset_x;
-    qoffset_sc(1,3)= mask_100_nii.hdr.hist.qoffset_y;
-    qoffset_sc(1,4)= mask_100_nii.hdr.hist.qoffset_z;
-% meshing
-for i=1:3
-    output_mha_mask.info.Dimensions(1,i) = mask_100_nii.hdr.dime.dim(1,1+i); %mesh global dimensions
-    output_mha_mask.info.Offset(1,i) = qoffset_sc(1,i+1); %mesh Offset
-    output_mha_mask.info.PixelDimensions(1,i) = mask_100_nii.hdr.dime.pixdim(1,1+i); %mesh pixel dimensions
+for i=1:v_max
+    switch geometry{i}
+        case '3D'
+            if i==biggest_ROI
+                finame=strcat('big_testROI',num2str(i));
+            else
+                finame=num2str(i);
+            end
+        case 'flat_plate'
+            finame=strcat('flat_plate',num2str(i));
+    end
+    finame=strcat(finame,'.nii');
+    mask_100_nii.img=single_ROIsmask{1,i};
+    save_nii(mask_100_nii,finame);
 end
+
+
+
+
+% %% create mha-template 
+% load('template_mha_mask.mat');
+% output_mha_mask= template_mha_mask;
+% %clear template_mha_mask;
+% output_mha_mask.mask = all_single_ROIs{1,1};
+% %prepare meshing
+%     qoffset_sc(1,2)= mask_100_nii.hdr.hist.qoffset_x;
+%     qoffset_sc(1,3)= mask_100_nii.hdr.hist.qoffset_y;
+%     qoffset_sc(1,4)= mask_100_nii.hdr.hist.qoffset_z;
+% % meshing
+% for i=1:3
+%     output_mha_mask.info.Dimensions(1,i) = mask_100_nii.hdr.dime.dim(1,1+i); %mesh global dimensions
+%     output_mha_mask.info.Offset(1,i) = qoffset_sc(1,i+1); %mesh Offset
+%     output_mha_mask.info.PixelDimensions(1,i) = mask_100_nii.hdr.dime.pixdim(1,1+i); %mesh pixel dimensions
+% end
 
 
 %% save ROIs as single Masks
---
+++ Matthias/02-Patients3MBq/testtoolbox_medcial_image_processing.m
@@ -0,0 +1,1 @@
+test=niftiread('Maske 100.nii');
