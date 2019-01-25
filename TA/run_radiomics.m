a104f257ffca66ac90e21fd0d0225fe084aacc28
OcelotHero <32492692+OcelotHero@users.noreply.github.com>
2019-01-25 11:49:13 +0100

    Updated script with bug fixes
    Minor code efficiency improvement.
    Script now takes into account volume image modality
    Added optional feature to take into account for non-isometric volume image.

--- TA/run_radiomics.m
+++ TA/run_radiomics.m
@@ -1,10 +1,12 @@
 function texture_metric_radiomics = run_radiomics(sPathImg, lMask,category)
 
+%%% Minor code efficiency fix
+%%% Fix bugs (feature calculation now takes into account image modality)
+%%% Added optional feature (non-isometric volume image to isometric)
 % settings for preprocessing
 % load volume
-isMatFile = strfind(sPathImg,'.mat');
-if isempty(isMatFile)
-    if exist(fullfile(sPathImg,'VOLUME_IMAGE.mat'))
+if ~contains(sPathImg,'.mat')
+    if exist(fullfile(sPathImg,'VOLUME_IMAGE.mat'), 'file')
         volume = load(fullfile(sPathImg,'VOLUME_IMAGE.mat'));
     else
         dicom23D(sPathImg);
@@ -22,11 +24,39 @@ end
 % mask_temp = main_read_mask(sPathMask);
 % mask = rot90(fliplr(mask_temp));
 
-scanType = 'MRscan';
+scanType = strcat(slice_data(1).Modality, 'scan');
+
+%%% ========
+% Comment/Uncomment if texture feature is to be calculated from volume
+% image as is
 pixelW = 1;
 sliceS = 1;
-R = 1; % no bandpass filtering
 scale = 'pixelW';
+
+%%% ========
+% Comment/Uncomment if texture feature is to be calculated from isometric 
+% volume
+%
+% pixelW = slice_data(1).PixelSpacing(1);
+% sliceS = slice_data(1).SliceThickness;
+% scale = 1; % scale to isometric volume with 1x1x1 mm spacing
+%
+% scale = 'pixelW' % scale to isometric volume with spacing matched to pixel
+%                  % width dimension
+%
+% scale = max(pixelW, sliceS) % isometric upsampling to largest dimension
+%
+% scale = min(pixelW, sliceS) % isometric downsampling to smallest
+%                             % dimension
+
+%%% ========
+% Old Implementation
+%
+% scanType = 'MRscan';
+% pixelW = 1;
+% sliceS = 1;
+% scale = 'pixelW';
+R = 1; % no bandpass filtering
 textType = 'Matrix'; % 'Global'
 quantAlgo = 'Equal'; 
 %              Either 'Equal' for equal-probability quantization, 'Lloyd'
@@ -36,8 +66,6 @@ Ng = 64;
 
 [ROIonly,levels] = prepareVolume(volume,lMask,scanType,pixelW,sliceS,R,scale,textType,quantAlgo,Ng);
 [ROIonly_Global] = prepareVolume(volume,lMask,scanType,pixelW,sliceS,R,scale,'Global');
-
-
 
 % texture features
 texture_metric_radiomics = fTexture_radiomics(ROIonly,ROIonly_Global,levels,category);
